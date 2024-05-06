3af35edbf2b8f41d35ab76350e1a1a2160fd3738
MemeLeader <124880624+MemeLeader@users.noreply.github.com>
2024-05-06 22:02:48 +0300

    Add files via upload

---
+++ demo/src/main/java/org/example/demo/HelloController.java
@@ -0,0 +1,22 @@
+package org.example.demo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import java.awt.*;
+
+
+public class HelloController {
+    @FXML
+    private TextField display;
+    @FXML
+    protected void onTypeNum(){
+
+            display.setText("hgfhfgh");
+
+    }
+    @FXML
+    private Button num1;
+
+}
